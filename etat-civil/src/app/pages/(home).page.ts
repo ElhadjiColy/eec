235abae14fc455831179c6a6b9b784c77f3b7973
Ibra <nfallyibrahimacoly@gmail.com>
2024-05-05 20:17:09 +0200

    Initial commit

---
+++ etat-civil/src/app/pages/(home).page.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+import { AnalogWelcomeComponent } from './analog-welcome.component';
+
+@Component({
+  selector: 'etat-civil-home',
+  standalone: true,
+  imports: [AnalogWelcomeComponent],
+  template: `
+     <etat-civil-analog-welcome/>
+  `,
+})
+export default class HomeComponent {
+}
